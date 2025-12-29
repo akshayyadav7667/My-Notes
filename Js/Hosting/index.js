40dc184dec108b3068583fa91ab56f3fb4570c57
Akshay Kumar <akshaykumar880466@gmail.com>
2025-12-30 04:22:09 +0530

    Hosting

--- Js/Hosting/index.js
+++ Js/Hosting/index.js
@@ -7,7 +7,11 @@ function a() {
   var x = 10; // local scope because of separate execution context
   console.log(x);
 }
-function b() {
+function b() {Ho
   var x = 100;
   console.log(x);
 }
+
+
+//  hosting is an concept which enables to extract values form the variables and function even before
+// assigning values without getting any error and this is happening due to the 1st phase of memory creation phase of execuation context .
